eb89e42efb9e0114041fb7a4ba887f6ffd651f90
Willy <willychai05@gmail.com>
2018-08-17 13:35:33 +0700

    fix header menu

--- user/header.php
+++ user/header.php
@@ -3,7 +3,7 @@
 	<input type="text" name="search" placeholder="Cari produk" class="search">
 	<a href="index.php"><button>Home</button></a>
 	<div class="w3-dropdown-hover">
-      <button>Kategori</button>
+		<button>Kategori</button>
       <div class="w3-dropdown-content w3-card-4" style="width: 200px; transition: 0.5s;">
       	<?php
 			$sql = "SELECT * FROM category";
@@ -11,7 +11,7 @@
 			while ($row = $query->fetch_assoc()) {
 				if($row['idCategory'] != 1){
 		?>		
-				<a href="#" style="width: 100%"><?php echo ucfirst($row['name']); ?></a>
+				<a href="user/category.php?id=<?php echo $row['idCategory']; ?>" style="width: 100%"><?php echo ucfirst($row['name']); ?></a>
 		<?php
 				}
 			}
@@ -23,8 +23,7 @@
 </div>
 		<div class="menuProfil w3-card-4 out" id="menuProfil" onmouseover="menuProfilIn()" onmouseout="menuProfilOut()">
 			<a href="keranjang.php"><button>Keranjang</button></a><br>
-			<a href="konfirmasiPesanan.php"><button>Konfirmasi Pesanan</button></a><br>
-			<a href="statusPengiriman.php"><button>Status Pengiriman</button></a><br>
+			<a href="confirmation.php"><button>Konfirmasi Pesanan</button></a><br>
 			<a href="kontak.php"><button>Pengaturan Kontak</button></a><br>
 			<a href="#" onclick="signOut();"><button>Log Out</button></a>
 		</div>
