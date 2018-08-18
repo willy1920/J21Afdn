e10c096d65a3afffeb0ef8566df9ff4648cc0647
apriadi27 <apriadiwijaya9e3@gmail.com>
2018-08-18 19:32:33 +0700

    fix admin menu and design

--- admin/menu.php
+++ admin/menu.php
@@ -5,6 +5,5 @@
         <a href="category.php"><button>Kategori</button></a>
         <a href="sosmed.php"><button>Akun</button></a>
         <a href="payment.php"><button>Konfirmasi Pembayaran</button></a>
-        <a href="sale.php"><button>Promo</button></a>
         <a href="index.php?logout=true"><button style="float: right;">Logout</button></a>
     </div>
